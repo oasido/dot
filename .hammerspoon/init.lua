92999cba49a3cc7c37401d0a95e99fb5d5a86131
oasido <asido.ofek@gmail.com>
2025-11-18 18:11:39 +0200

    update through script - 2025-11-18 18:11:39

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,19 @@
+-- detect display configuration changes
+function updateDockPosition()
+	-- get count of all screens (built-in + external)
+	local screenCount = #hs.screen.allScreens()
+
+	-- if only one screen, dock on left
+	-- if more than one screen, dock on bottom
+	if screenCount == 1 then
+		hs.execute("defaults write com.apple.dock orientation left && killall Dock")
+	else
+		hs.execute("defaults write com.apple.dock orientation bottom && killall Dock")
+	end
+end
+
+-- run when displays are connected or disconnected
+hs.screen.watcher.new(updateDockPosition):start()
+
+-- run once on startup
+updateDockPosition()
