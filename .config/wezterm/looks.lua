a5cbd553432d92f68387ebe071e42b7ac55803e1
oasido <asido.ofek@gmail.com>
2026-01-25 10:23:04 +0200

    update through script - 2026-01-25 10:23:04

---
+++ .config/wezterm/looks.lua
@@ -0,0 +1,27 @@
+local wezterm = require("wezterm")
+
+local function with_looks(config)
+	config.color_scheme = "Eighties (base16)"
+	config.hide_tab_bar_if_only_one_tab = true
+	config.window_decorations = "RESIZE"
+	config.window_padding = {
+		left = 8,
+		right = 8,
+		top = 8,
+		bottom = 8,
+	}
+
+	config.bidi_enabled = true
+
+	config.line_height = 1.2
+
+	config.font = wezterm.font({
+		family = "IosevkaTerm Nerd Font Mono",
+	})
+
+	config.native_macos_fullscreen_mode = true
+
+	config.font_size = 21.0
+end
+
+return with_looks
